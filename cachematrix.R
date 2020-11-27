b6a0149bdb05a0e58096e6e225f4e630196ff8ae
Daniela <daniela.chavez@gmail.com>
2020-11-27 11:53:28 -0600

    Archivo con cambios ejecutados para poder entregar el commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+##Es muy similar a la función makeVector, solo que en lugar de la media se cambia por la inversa
+##La misma asignación comenta que la función que devuelve la inversa de la matriz es la función solve ()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
+#Se realizaron un par de ejercicios para confirmar que en efecto devuelve la inversa de la matriz.
+        
